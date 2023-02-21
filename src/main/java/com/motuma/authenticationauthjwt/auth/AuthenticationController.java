a789444b6664d7091aec7fca774e897aa05b7536
motumag <motumagishu27@gmail.com>
2023-02-21 14:30:15 +0300

    changes

--- src/main/java/com/motuma/authenticationauthjwt/auth/AuthenticationController.java
+++ src/main/java/com/motuma/authenticationauthjwt/auth/AuthenticationController.java
@@ -1,7 +1,9 @@
 package com.motuma.authenticationauthjwt.auth;
 
+import com.motuma.authenticationauthjwt.config.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService service;
+    private final JwtService jwtService;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
-    return ResponseEntity.ok(service.register(request));
+        return ResponseEntity.ok(service.register(request));
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+//        RefreshToken refreshToken = refreshTokenService.createRefreshToken(userIdcontains);
+//        System.out.println(refreshToken);
         return ResponseEntity.ok(service.authenticate(request));
     }
+
 }
--- src/main/java/com/motuma/authenticationauthjwt/auth/AuthenticationService.java
+++ src/main/java/com/motuma/authenticationauthjwt/auth/AuthenticationService.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
